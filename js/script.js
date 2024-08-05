5b1018a57897dcf751a360d721f128615c01ec1f
Paweł <p.grabczak@cognitionhub.com>
2024-08-05 17:15:13 +0200

    Displaying the article upon clicking

--- js/script.js
+++ js/script.js
@@ -8,30 +8,38 @@
 const titleClickHandler = function () {
     console.log('Link was cliked!');
     console.log(event)
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for  (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    /* [DONE] add class 'active' to the clicked link */
+    event.preventDefault();
+    const clickedElement = this; //nie dokońca romuzmiem działanie this
     console.log('clickedElement:', clickedElement);
-    
-    const clickedElement = this;
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('active');
+    const activeArticles = document.querySelectorAll('.posts .post.active');
 
     for  (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
-    /* add class 'active' to the correct article */
-}
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    targetArticle.classList.add('active');
+
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+} //ból i cierpienie
 
 const links = document.querySelectorAll('.titles a');
 
